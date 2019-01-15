37ed097e78b939739e34d7fa7358896bd6fb8b5c
yulu04 <yulu04@baidu.com>
2019-01-15 11:32:57 +0800

    crf

--- cnlp/model/crf.py
+++ cnlp/model/crf.py
@@ -8,6 +8,8 @@ Desc:
 Y_START_LABEL = '*label_start*'
 Y_NONE_LABEL = '*label_none*'
 
+import numpy as np
+
 
 class Crf(object):
     features_num = 0  # 用于生成feature_id
@@ -26,7 +28,7 @@ class Crf(object):
         self.features_num = 0  # 用于生成feature_id
         self.features_dict = {}  # { x_feature : { y_feature : feature_id } }
         self.features_count = {}  # { feature_id : counts }
-        self.labels_dict = {}  # 特征标签dict { y_feature: y_feature_id}
+        self.labels_dict = {}  # 特征标签dict { y : y_id}
         pass
 
     # 预测标记
@@ -42,14 +44,17 @@ class Crf(object):
         pass
 
     # 计算转移概率矩阵
-    def generate_trans_matrix(self, X):
+    def generate_X_trans_matrix(self, X):
         pass
 
-    # 计算转移概率
-    def generate_trans_prob(self, X, t):
-        pass
+    # 计算条件概率
+    def generate_x_trans_matrix(self, weights, X, t):
+        x_trans_matrix = np.zeros(len(self.labels_dict), len(self.labels_dict))
+        x_feature_funcs = self.get_feature_funcs_from_dict(X, t)
+        for (prev, y), feature_ids in x_feature_funcs.items():
+            pass
 
-    # 计算前向概率、后向概率和归一化银子
+    # 计算前向概率、后向概率和归一化因子
     def forward_backward(self, X):
         pass
 
@@ -65,9 +70,12 @@ class Crf(object):
     def load_model(self):
         pass
 
-    # 根据训练数据生成对应的特征函数表示形式
-    def data2features(self, data):
-        # 生成用于辅助计算特征函数的变量
+    def generate_feature_dict(self, data):
+        """
+        生成特征函数词典和标记的词典，用于辅助计算
+        :param data:
+        :return:
+        """
         labels_num = 0
         for X, Y in data:
             for t in range(len(X)):
@@ -75,19 +83,26 @@ class Crf(object):
                 y = Y[t]
                 y_prev = Y[t - 1] if t > 0 else Y_START_LABEL
                 y_features = [(y_prev, y), (Y_NONE_LABEL, y)]
-                # 生成特征函数的词典
+
+                # 生成特征函数的词典, 形式为 { x_feature : { y_feature : feature_id } }
                 self.fill_features_dict(x_features, y_features)
 
+                # 生成状态词典, 形式为 { y : y_id }
                 if y not in self.labels_dict:
                     self.labels_dict[y] = labels_num
                     labels_num += 1
 
+    def data2features(self, data):
+        """
+        将观测序列数据转换成特征函数的表示形式, m个样本, 每个样本长度为T, 每个时刻有K个特征函数
+        一组特征函数的形式为:  { (y_prev, y) : feature_ids }
+        :param data:
+        :return:
+        """
         data_features = []
         for X, _ in data:
             data_features.append([self.get_feature_funcs_from_dict(X, t) for t in range(len(X))])
 
-        # data_features形状为: m个样本, 每个样本时序长度为T, 每个时刻有K个特征函数
-        # K个特征函数的形式为:  { (y_prev, y) : feature_ids }
         return data_features
 
     def fill_features_dict(self, x_features, y_features):
@@ -114,8 +129,13 @@ class Crf(object):
                     feature_id = self.features_dict[x_feature][y_feature]
                     self.features_count[feature_id] += 1
 
-    # 从特征词典中获取X序列在t时刻的特征函数集合 { (y_prev, y) : feature_ids }
     def get_feature_funcs_from_dict(self, X, t):
+        """
+        根据特征词典提取获观测序列X在t时刻对应的特征函数集合: { (y_prev, y) : feature_ids }
+        :param X:
+        :param t:
+        :return:
+        """
         feature_funcs = {}
 
         x_features = self.get_x_features_from_template(X, t)
@@ -124,13 +144,16 @@ class Crf(object):
                 if (y_prev, y) not in feature_funcs:
                     feature_funcs[(y_prev, y)] = set()
                 feature_funcs[(y_prev, y)].add(feature_id)
-        print(feature_funcs)
-        exit()
         return feature_funcs
 
-    # 从特征函数模板中获取序列X在t时刻的x特征集合([x_feature1, x_feature2, ...])
     @staticmethod
     def get_x_features_from_template(X, t):
+        """
+        从特征函数模板中获取观测序列X在t时刻的x特征集合([x_feature1, x_feature2, ...])
+        :param X:
+        :param t:
+        :return:
+        """
         length = len(X)
         x_features = list()
         x_features.append('U[0]:%s' % X[t][0])
